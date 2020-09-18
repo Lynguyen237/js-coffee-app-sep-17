cd3a20386a62889e269a8a40637b67a7c62fd382
Ly Nguyen <lynguyen237@gmail.com>
2020-09-18 04:12:45 +0000

    finish'

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -8,6 +8,7 @@ const addItemToCart = (itemName) => {
   `);
 };
 
+
 const resetCart = () => {
   $('#cart-total').html('0.00');
   $('#cart-items').empty();
@@ -38,3 +39,59 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+
+// WAY 1 - not using jQuery
+// const button = document.querySelector('.add-to-order');
+
+//   button.addEventListener('click', () => {
+//     alert('Stop clicking me!');
+//     console.log();
+// });
+// End of Way 1 - not using jQuery
+
+
+// WAY 2 = Using jQuery the long way
+// const button = $('.add-to-order');
+
+// button.on('click', () => {
+//   alert ('Handled with jQuery');
+// });
+// End of WAY 2 = Using jQuery the long way
+
+// Way 3
+$('.add-to-order').on('click', () => {
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+});
+// End Way 3
+
+
+// Way 1 to update # of coffee sold = Using jQuery the short way
+// id use '#id and class use '.class'
+// $('#place-order').on('click', () => {
+//   incrementCoffeeSold($('#cart-items').children().length);
+//   resetCart();
+// });
+// End Way 1 to update # of coffee sold = Using jQuery the short way
+
+
+// WAY 2 to update # of coffee sold = Using jQuery the long way
+// const button = $('#place-order');
+
+// button.on('click', () => {
+//   incrementCoffeeSold($('#cart-items').children().length);
+//   resetCart();
+// })
+// End WAY 2 to update # of coffee sold
+
+
+// Way 3 to update # of coffee sold = Not using jQuery
+const button = document.querySelector('#place-order');
+    button.addEventListener('click', () => {
+      incrementCoffeeSold($('#cart-items').children().length);
+      resetCart();
+});
+
+
+
+
